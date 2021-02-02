9f65581ba9a08e10f8b019edd85d7e12f62136f1
shash3 <shashank.p0713@gmail.com>
2021-02-01 19:19:44 -0500

    Login and Registration forms

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,23 @@
 import './App.scss';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import  Navbar  from "./components/Navbar.js";
+import  Home  from "./components/Home.js";
+import  Login  from "./components/auth/Login.js";
+import  Register  from "./components/auth/Register.js";
 
 
 function App() {
   return (
-    <div>
+    <Router>
       <Navbar />
-    </div>
+      <Route exact path='/' component={Home}/>
+      <section className="container">
+        <Switch>
+          <Route exact path='/login' component={Login}/>
+          <Route exact path='/register' component={Register}/>
+        </Switch>
+      </section>
+    </Router>
   );
 }
 
