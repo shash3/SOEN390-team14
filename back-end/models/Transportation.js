6b06df5d559811314b3ca9cdd4c211ce17ba5d71
j-eltayar <eltayarjames@gmail.com>
2021-02-23 21:52:59 -0500

    update

--- back-end/models/Transportation.js
+++ back-end/models/Transportation.js
@@ -1,6 +1,10 @@
 const mongoose = require('mongoose');
 
 const TransportationSchema = new mongoose.Schema({
+    iid: {
+        type: String,
+        required: true
+    },
     name: {
         type: String,
         required: true
