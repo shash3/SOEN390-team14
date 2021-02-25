bb410eba2efe46ae41ccbd0586b5a755f45366b5
Sacha Elkaim <sachaelkaim87@gmail.com>
2021-02-24 22:17:02 -0500

    update

--- back-end/test/UsersTest/authTest.test.js
+++ back-end/test/UsersTest/authTest.test.js
@@ -1,13 +1,28 @@
 const request = require("supertest");
 const app = require("../../server.js");
 
+let token = "";
+
 describe("Post Endpoints", () => {
   it("test logging in ", async () => {
     const res = await request(app).post("/api/auth/login").send({
       email: "admin@gmail.com",
       password: "12345678",
     });
+    token = res.body.token;
     expect(res.body.token).toBeTruthy();
     expect(res.body.permission).toBeTruthy();
+  });
+
+  it("retrieve user by name ", async () => {
+    const res1 = await request(app).post("/api/auth/name", {
+        headers: {
+          "x-auth-token": token,
+        },
+      }).send({
+        name: "admin"
+      });
+      console.log(res1.body)
+      expect(res1.body).toBeTruthy();
   });
 });
