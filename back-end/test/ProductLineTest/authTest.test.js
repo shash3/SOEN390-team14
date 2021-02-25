3731fc2fbe6370d5d8722f4e3b84419db45a064d
DerekRuiz <47483376+DerekRuiz@users.noreply.github.com>
2021-02-24 23:35:34 -0500

    Unit Tests For Product Line

---
+++ back-end/test/ProductLineTest/authTest.test.js
@@ -0,0 +1,51 @@
+const request = require("supertest");
+const app = require("../../server.js");
+
+let token = "";
+describe("Post Endpoints", () => {
+    it("test logging in ", async () => {
+        const res = await request(app).post("/api/auth/login").send({
+          email: "admin@gmail.com",
+          password: "12345678",
+        });
+        token = res.body.token;
+        console.log(token)
+        expect(res.body.token).toBeTruthy();
+        expect(res.body.permission).toBeTruthy();
+      });
+  
+    it("retrieve product line by name ", async () => {
+      const res1 = await request(app).post("/api/product_line", {
+          headers: {
+            "x-auth-token": token,
+          },
+        }).send({
+          name: "Saddle",
+          material:[["Leather",1],["Seat Rod",1],["Seat Frame",1],["Sponge",1],["Steel Spring",2],["Clamp",1],["Screws",6]]
+        });
+        expect(res1.body).toBeTruthy();
+    });
+
+    it("Getting all product lines ", async () => {
+        const res1 = await request(app).get("/api/product_line/", {
+            headers: {
+              "x-auth-token": token,
+            },
+          }).send({
+            name: "Saddle",
+            material:[["Leather",1],["Seat Rod",1],["Seat Frame",1],["Sponge",1],["Steel Spring",2],["Clamp",1],["Screws",6]]
+          });
+          expect(res1.body).toBeTruthy();
+      });
+
+      it("Adding new product line ", async () => {
+        const res1 = await request(app).post("/api/product_line/add", {
+            headers: {
+              "x-auth-token": token,
+            },
+          }).send({
+            name: "New Prod"
+          });
+          expect(res1.body).toBeTruthy();
+      });
+  });
