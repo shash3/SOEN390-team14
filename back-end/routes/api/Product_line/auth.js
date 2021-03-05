98aa411b89093d982107d5d39da017828b080fbf
DerekRuiz <47483376+DerekRuiz@users.noreply.github.com>
2021-03-04 21:43:25 -0500

    Completed Product Line Creation
    Can create a new product line and save it to database.
    Error checking in place so no bad data is saved to database.

--- back-end/routes/api/Product_line/auth.js
+++ back-end/routes/api/Product_line/auth.js
@@ -33,9 +33,10 @@ router.post("/", auth, async (req, res) => {
 
 // add new product line
 router.post("/add", async (req, res) => {
-  const { name } = req.body;
+  const { name, material } = req.body;
   product = new Product_line({
-    name
+    name,
+    material
   });
   await product.save();
 });
