4de4dddde32f01a9aa0b0935b55c4b87cdbc0582
DerekRuiz <47483376+DerekRuiz@users.noreply.github.com>
2021-03-07 14:27:26 -0500

    Quality Assurance Backend
    Added backend support for quality assurance database and API

---
+++ back-end/models/Quality.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+const QualitySchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    location: {
+        type: String,
+        required: true
+    },
+    quality: {
+        type: String,
+        required: true
+    },
+});
+
+module.exports = Quality = mongoose.model('quality', QualitySchema);
