99cd1a97b1c95bfebdfe9dda8c273b03cf792a2e
DerekRuiz <47483376+DerekRuiz@users.noreply.github.com>
2021-03-09 21:33:00 -0500

    Created Interface For Scheduling
    Scheduling has a basic interface for the machines in a plant. It also has testing controls.

--- client/src/views/components/Production_Scheduling.js
+++ client/src/views/components/Production_Scheduling.js
@@ -7,32 +7,12 @@ import {
   Card,
   CardHeader,
   CardFooter,
-  DropdownMenu,
-  DropdownItem,
-  UncontrolledDropdown,
-  DropdownToggle,
   Media,
-  Pagination,
-  PaginationItem,
-  PaginationLink,
-  Progress,
   Table,
   Container,
   Row,
-  Form,
-  FormGroup,
-  InputGroup,
-  InputGroupAddon,
-  InputGroupText,
-  Input,
   ButtonGroup,
   Button,
-  Modal,
-  ModalHeader,
-  ModalBody,
-  ModalFooter,
-  FormFeedback,
-  Label,
 } from "reactstrap";
 
 // core components
@@ -41,14 +21,78 @@ import Header from "components/Headers/Header.js";
 const Production_Scheduling = (props) => {
   const userToken = JSON.parse(localStorage.getItem("user"));
 
+  const [userLocation, setUserLocation] = useState("");
   const [machines, setMachines] = useState([]);
 
   const NUM_OF_COLS = 2;
 
+  // Retrieve product line location from user
+  const getMachines = async () => {
+    const response = await axios.post("/api/machine/location",
+    {
+      location: userLocation
+    },
+    {
+      headers: {
+        "x-auth-token": userToken,
+      },
+    })
+    .catch((err) => console.log("Error", err));
+    if (response && response.data) {
+      var machines = response.data;
+      setMachines(machines);
+    }
+  };
+
+
+  const addItemToMachine = async (key, item) => {
+    const final = new Date();
+    final.setMinutes(new Date().getMinutes() + 5);
+
+    await axios.put("/api/machine/add",
+    {
+      _id:key,
+      item:item,
+      finish_time:final.toISOString(),
+    },
+    {
+      headers: {
+        "x-auth-token": userToken,
+      },
+    })
+    .then((response) =>{
+      getMachines();
+    })
+    .catch((err) => console.log("Error", err)); 
+  }
+
+  const removeItemFromMachine = async (key) => {
+    const final = new Date();
+    final.setMinutes(new Date().getMinutes() + 5);
+
+    await axios.put("/api/machine/remove",
+    {
+      _id:key,
+    },
+    {
+      headers: {
+        "x-auth-token": userToken,
+      },
+    })
+    .then((response) =>{
+      getMachines();
+    })
+    .catch((err) => console.log("Error", err));
+    
+  }
+
+
+
+
   // Retrieve values only once.
-  useEffect(() => {
+  useEffect( () => {
     // Retrieve product line location from user
-    const getProdLoc = async () => {
+    const getUserLoc = async () => {
       const response = await axios.get("/api/auth", 
       {
         headers: {
@@ -56,33 +100,23 @@ const Production_Scheduling = (props) => {
         },
       })
       .catch((err) => console.log("Error", err));
+      
       if (response && response.data) {
         var user = response.data;
         setUserLocation(user.location);
+        
       }
     };
-    getProdLoc();
 
-    // Retrieve product line location from user
-    const getMachines = async () => {
-      const response = await axios.get("/api/location",
-      {
-        location: u
-      },
-      {
-        headers: {
-          "x-auth-token": userToken,
-        },
-      })
-      .catch((err) => console.log("Error", err));
-      if (response && response.data) {
-        var user = response.data;
-        setProdLoc(user.location);
-      }
-    };
-    getMachines();
+     getUserLoc();
   }, []);
 
+
+    // Retrieve values only once.
+    useEffect( () => {
+      
+      getMachines();
+    }, [userLocation]);
 
   /* -------------------------
    * Returns the HTML code for the productino tab.
@@ -93,39 +127,71 @@ const Production_Scheduling = (props) => {
       <Header />
       {/* Page content */}
       <Container className="mt--7" fluid>
-
         {machines.map((m, i) => (
-          <div className='row'>
-            
+          <div>
             <Row>
               <div className="col">
                 <Card className="shadow">
                   <CardHeader className="border-0">
-                    <h2 className="mb-0">Machine #{i}</h2>
+                    <h2 className="mb-0">Machine #{i+1}</h2>
+                    <Badge color="" className="badge-dot mr-4">
+                      <i className={m.item == "" ? "bg-success" : "bg-danger"} />
+                      {m.item == "" ? "Available" : "Unavailable"}
+                    </Badge>
                   </CardHeader>
-
-                  <Media className="align-items-center">
-                    <Media>
-                      <span className="mb-0 text-sm">{m.location}</span>
-                    </Media>
-                  </Media>
-                  <Media className="align-items-center">
-                    <Media>
-                      <span className="mb-0 text-sm">{m.item}</span>
-                    </Media>
-                  </Media>
-                  <Media className="align-items-center">
-                    <Media>
-                      <span className="mb-0 text-sm">{m.date}</span>
-                    </Media>
-                  </Media>
-                  <Badge color="" className="badge-dot mr-4">
-                    <i className={m.item == "" ? "bg-success" : "bg-danger"} />
-                    {m.item == "" ? "Available" : "Unavailable"}
-                  </Badge>
-
-                  <CardFooter className="py-4">
-    
+                  <Table className="align-items-center table-flush" responsive>
+                    <thead className="thead-light">
+                      <tr>
+                        <th scope="col">Location</th>
+                        <th scope="col">{m.item == "" ? "" :"Product Name"}</th>
+                        <th scope="col">{m.item == "" ? "" :"Date Finished"}</th>
+                        <th scope="col" />
+                      </tr>
+                      <tr key={"Machine #" + (i+1)}>
+                        <td scope="row">
+                          <Media className="align-items-center">
+                            <Media>
+                              <span className="mb-0 text-sm">{m.location}</span>
+                            </Media>
+                          </Media>
+                        </td>
+                        <td>
+                          <Media className="align-items-center">
+                            <Media>
+                              <span className="mb-0 text-sm">{m.item == "" ? "" : m.item}</span>
+                            </Media>
+                          </Media>
+                        </td>
+                        <td>
+                          <Media className="align-items-center">
+                            <Media>
+                              <span className="mb-0 text-sm">{m.item == "" ? "" : m.finish_time}</span>
+                            </Media>
+                          </Media>
+                        </td>
+                      </tr>
+                    </thead>
+                  </Table>
+                  <CardFooter className="py-2 pb-3">
+                    <ButtonGroup className='px-3'>
+                      <Button 
+                        className="mt-4"
+                        color="primary"
+                        onClick={(e) => addItemToMachine(m._id, "Saddle")}
+                      >
+                        Add Saddle (testing)
+                      </Button>
+                    </ButtonGroup>
+                      
+                    <ButtonGroup className='px-3'>
+                      <Button 
+                        className="mt-4"
+                        color="primary"
+                        onClick={(e) => removeItemFromMachine(m._id)}
+                      >
+                        Abort Process
+                      </Button>
+                    </ButtonGroup>
                   </CardFooter>
                 </Card>
               </div>
@@ -134,9 +200,6 @@ const Production_Scheduling = (props) => {
             <br/>
           </div>
         ))}
-
-
-        
       </Container>
 
     </>
