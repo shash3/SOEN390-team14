aee9aa3de9eed2c4ab8cf272ee6649150f19be5e
DerekRuiz <47483376+DerekRuiz@users.noreply.github.com>
2021-03-13 16:34:07 -0500

    Converted To Coding Convention

--- client/src/layouts/Admin.js
+++ client/src/layouts/Admin.js
@@ -1,82 +1,91 @@
-import React from "react";
-import { useLocation, Route, Switch, Redirect } from "react-router-dom";
+/* eslint-disable react/no-array-index-key */
+/* eslint-disable react/destructuring-assignment */
+/* eslint-disable react/jsx-props-no-spreading */
+/* eslint-disable react/prop-types */
+/* eslint-disable no-undef */
+import React from 'react';
+import {
+  useLocation, Route, Switch, Redirect,
+} from 'react-router-dom';
 // reactstrap components
-import { Container } from "reactstrap";
+import { Container } from 'reactstrap';
 // core components
-import AdminNavbar from "components/Navbars/AdminNavbar.js";
-import AdminFooter from "components/Footers/AdminFooter.js";
-import Sidebar from "components/Sidebar/Sidebar.js";
-import Index from "views/Index.js";
-import Profile from "views/components/Profile.js";
-import Transportation from "views/components/Transportation.js";
-import Register from "views/components/Register.js";
-import Login from "views/components/Login.js";
-import Finance from "views/components/Finance.js";
-import Production from "views/components/Production.js";
-import QualityAssurance from "views/components/QualityAssurance.js";
-import Production_Scheduling from "views/components/Production_Scheduling.js";
+import AdminNavbar from '../components/Navbars/AdminNavbar';
+import AdminFooter from '../components/Footers/AdminFooter';
+import Sidebar from '../components/Sidebar/Sidebar';
+import Index from '../views/Index';
+import Profile from '../views/components/Profile';
+import Transportation from '../views/components/Transportation';
+import Register from '../views/components/Register';
+import Login from '../views/components/Login';
+import Finance from '../views/components/Finance';
+import Production from '../views/components/Production';
+import QualityAssurance from '../views/components/QualityAssurance';
+import ProductionScheduling from '../views/components/Production_Scheduling';
+
+const logoImg = require('../assets/img/brand/logo.png');
 
 const Admin = (props) => {
   // IN ROUTES.JS AS WELL IF MODIFYING
-  var routes = [
+  const routes = [
     {
-      path: "/index",
-      name: "Dashboard",
-      icon: "ni ni-tv-2 text-primary",
+      path: '/index',
+      name: 'Dashboard',
+      icon: 'ni ni-tv-2 text-primary',
       component: Index,
-      layout: "/admin",
+      layout: '/admin',
     },
     {
-      path: "/production",
-      name: "Production",
-      icon: "ni ni-planet text-blue",
+      path: '/production',
+      name: 'Production',
+      icon: 'ni ni-planet text-blue',
       component: Production,
-      layout: "/admin",
+      layout: '/admin',
     },
     {
-      path: "/transportation",
-      name: "Transportation",
-      icon: "ni ni-pin-3 text-orange",
+      path: '/transportation',
+      name: 'Transportation',
+      icon: 'ni ni-pin-3 text-orange',
       component: Transportation,
-      layout: "/admin",
+      layout: '/admin',
     },
     {
-      path: "/finance",
-      name: "Finance",
-      icon: "ni ni-bullet-list-67 text-red",
+      path: '/finance',
+      name: 'Finance',
+      icon: 'ni ni-bullet-list-67 text-red',
       component: Finance,
-      layout: "/admin",
+      layout: '/admin',
     },
     {
-      path: "/quality-assurance",
-      name: "Quality Assurance",
-      icon: "ni ni-bullet-list-67 text-red",
+      path: '/quality-assurance',
+      name: 'Quality Assurance',
+      icon: 'ni ni-bullet-list-67 text-red',
       component: QualityAssurance,
-      layout: "/admin",
+      layout: '/admin',
     },
     {
-      path: "/user-profile",
-      name: "User Profile",
-      icon: "ni ni-single-02 text-yellow",
+      path: '/user-profile',
+      name: 'User Profile',
+      icon: 'ni ni-single-02 text-yellow',
       component: Profile,
-      layout: "/admin",
+      layout: '/admin',
     },
     {
-      path: "/login",
-      name: "Login",
-      icon: "ni ni-key-25 text-info",
+      path: '/login',
+      name: 'Login',
+      icon: 'ni ni-key-25 text-info',
       component: Login,
-      layout: "/auth",
+      layout: '/auth',
     },
     {
-      path: "/register",
-      name: "Register",
-      icon: "ni ni-circle-08 text-pink",
+      path: '/register',
+      name: 'Register',
+      icon: 'ni ni-circle-08 text-pink',
       component: Register,
-      layout: "/auth",
+      layout: '/auth',
     },
   ];
-  const permission = JSON.parse(localStorage.getItem("permission"));
+  const permission = JSON.parse(localStorage.getItem('permission'));
   const mainContent = React.useRef(null);
   const location = useLocation();
 
@@ -86,62 +95,59 @@ const Admin = (props) => {
     mainContent.current.scrollTop = 0;
   }, [location]);
 
-  const getRoutes = (routes) => {
-    return routes.map((prop, key) => {
-      if (prop.layout === "/admin") {
-        return (
-          <Route
-            path={prop.layout + prop.path}
-            component={prop.component}
-            key={key}
-          />
-        );
-      } else {
-        return null;
-      }
-    });
-  };
+  const getRoutes = (inputRoutes) => inputRoutes.map((prop, key) => {
+    if (prop.layout === '/admin') {
+      return (
+        <Route
+          path={prop.layout + prop.path}
+          component={prop.component}
+          key={key}
+        />
+      );
+    }
+    return null;
+  });
 
-  const getBrandText = (path) => {
-    for (let i = 0; i < routes.length; i++) {
-      //console.log(routes.length);
+  const getBrandText = () => {
+    for (let i = 0; i < routes.length; i += 1) {
+      // console.log(routes.length);
       if (
-        props.location.pathname.indexOf(routes[i].layout + routes[i].path) !==
-        -1
+        props.location.pathname.indexOf(routes[i].layout + routes[i].path)
+        !== -1
       ) {
         return routes[i].name;
       }
     }
-    return "Brand";
+    return 'Brand';
   };
 
   // logic to remove nav items depending on permission
   const permissionRoutes = () => {
-    if (permission === "admin") {
-      var i;
-      for (i = 0; i < routes.length; i++) {
-        if (routes[i].name === "User Profile") {
-          routes[i].name = "Admin Panel";
-          routes[i].path = "/admin-panel";
+    if (permission === 'admin') {
+      let i;
+      for (i = 0; i < routes.length; i += 1) {
+        if (routes[i].name === 'User Profile') {
+          routes[i].name = 'Admin Panel';
+          routes[i].path = '/admin-panel';
         }
       }
     }
 
-    if (permission === "none" || permission === null) {
+    if (permission === 'none' || permission === null) {
       routes.splice(1, 4);
     }
-    if (permission === "production") {
+    if (permission === 'production') {
       routes.splice(2, 3);
     }
-    if (permission === "transportation") {
+    if (permission === 'transportation') {
       routes.splice(1, 1);
       routes.splice(2, 2);
     }
-    if (permission === "finance") {
+    if (permission === 'finance') {
       routes.splice(1, 2);
       routes.splice(2, 1);
     }
-    if (permission === "assurance") {
+    if (permission === 'assurance') {
       routes.splice(1, 3);
     }
     return routes;
@@ -153,9 +159,9 @@ const Admin = (props) => {
         {...props}
         routes={permissionRoutes()}
         logo={{
-          innerLink: "/admin/index",
-          imgSrc: require("../assets/img/brand/logo.png").default,
-          imgAlt: "...",
+          innerLink: '/admin/index',
+          imgSrc: logoImg.default,
+          imgAlt: '...',
         }}
       />
       <div className="main-content" ref={mainContent}>
@@ -164,16 +170,16 @@ const Admin = (props) => {
           brandText={getBrandText(props.location.pathname)}
         />
         <Switch>
-          {permission === "production" && (
+          {permission === 'production' && (
             <Route
               path="/admin/production-scheduling"
-              component={Production_Scheduling}
+              component={ProductionScheduling}
             />
           )}
-          {permission === "admin" && (
+          {permission === 'admin' && (
             <Route
               path="/admin/production-scheduling"
-              component={Production_Scheduling}
+              component={ProductionScheduling}
             />
           )}
           {getRoutes(routes)}
