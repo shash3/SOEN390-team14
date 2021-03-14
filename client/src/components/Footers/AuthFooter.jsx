5cc8ba45032412c3a9cd265b1f5ee98d99ca7813
DerekRuiz <47483376+DerekRuiz@users.noreply.github.com>
2021-03-13 23:21:44 -0500

    Refactor For Coding Convention

---
+++ client/src/components/Footers/AuthFooter.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+
+// reactstrap components
+import {
+  NavItem, NavLink, Nav, Container, Row, Col,
+} from 'reactstrap';
+
+const Login = () => (
+  <>
+    <footer className="py-5">
+      <Container>
+        <Row className="align-items-center justify-content-xl-between">
+          <Col xl="6">
+            <div className="copyright text-center text-xl-left text-muted">
+              ©
+              {new Date().getFullYear()}
+              {' '}
+              <a
+                className="font-weight-bold ml-1"
+                href="https://www.creative-tim.com?ref=adr-auth-footer"
+                rel="noreferrer"
+                target="_blank"
+              >
+                SOEN390-TEAM14
+              </a>
+            </div>
+          </Col>
+          <Col xl="6">
+            <Nav className="nav-footer justify-content-center justify-content-xl-end">
+              <NavItem>
+                <NavLink
+                  href="https://github.com/shash3/SOEN390-team14/"
+                  target="_blank"
+                >
+                  MINI CAPSTONE: BIKERR ERP SYSTEM
+                </NavLink>
+              </NavItem>
+            </Nav>
+          </Col>
+        </Row>
+      </Container>
+    </footer>
+  </>
+);
+
+export default Login;
---
+++ client/src/components/Headers/CardlessHeader.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+// reactstrap components
+import { Container, Row } from 'reactstrap';
+
+const Header = () => (
+  <>
+    <div className="header bg-gradient-info pb-8 pt-5 pt-md-8">
+      <Container fluid>
+        <div className="header-body">
+          {/* Card stats */}
+          <Row />
+        </div>
+      </Container>
+    </div>
+  </>
+);
+
+export default Header;
