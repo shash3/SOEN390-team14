875a05f51d80fb4f899a4194e93fbff49306bd6e
DerekRuiz <47483376+DerekRuiz@users.noreply.github.com>
2021-03-13 21:09:25 -0500

    Initial Refactoring For ESLint

--- client/src/routes.js
+++ client/src/routes.js
@@ -1,69 +1,69 @@
-import Index from "views/Index.js";
-import Profile from "views/components/Profile.js";
-import Transportation from "views/components/Transportation.js";
-import Register from "views/components/Register.js";
-import Login from "views/components/Login.js";
-import Finance from "views/components/Finance.js";
-import Production from "views/components/Production.js";
-import QualityAssurance from "views/components/QualityAssurance.js";
+import Index from './views/Index';
+import Profile from './views/components/Profile';
+import Transportation from './views/components/Transportation';
+import Register from './views/components/Register';
+import Login from './views/components/Login';
+import Finance from './views/components/Finance';
+import Production from './views/components/Production';
+import QualityAssurance from './views/components/QualityAssurance';
 
 // IN ADMIN.JS AS WELL IF MODIFYING
-var routes = [
+const routes = [
   {
-    path: "/index",
-    name: "Dashboard",
-    icon: "ni ni-tv-2 text-primary",
+    path: '/index',
+    name: 'Dashboard',
+    icon: 'ni ni-tv-2 text-primary',
     component: Index,
-    layout: "/admin",
+    layout: '/admin',
   },
   {
-    path: "/production",
-    name: "Production",
-    icon: "ni ni-planet text-blue",
+    path: '/production',
+    name: 'Production',
+    icon: 'ni ni-planet text-blue',
     component: Production,
-    layout: "/admin",
+    layout: '/admin',
   },
   {
-    path: "/transportation",
-    name: "Transportation",
-    icon: "ni ni-pin-3 text-orange",
+    path: '/transportation',
+    name: 'Transportation',
+    icon: 'ni ni-pin-3 text-orange',
     component: Transportation,
-    layout: "/admin",
+    layout: '/admin',
   },
   {
-    path: "/finance",
-    name: "Finance",
-    icon: "ni ni-bullet-list-67 text-red",
+    path: '/finance',
+    name: 'Finance',
+    icon: 'ni ni-bullet-list-67 text-red',
     component: Finance,
-    layout: "/admin",
+    layout: '/admin',
   },
   {
-    path: "/Quality-assurance",
-    name: "Quality Assurance",
-    icon: "ni ni-bullet-list-67 text-red",
+    path: '/Quality-assurance',
+    name: 'Quality Assurance',
+    icon: 'ni ni-bullet-list-67 text-red',
     component: QualityAssurance,
-    layout: "/admin",
+    layout: '/admin',
   },
   {
-    path: "/user-profile",
-    name: "User Profile",
-    icon: "ni ni-single-02 text-yellow",
+    path: '/user-profile',
+    name: 'User Profile',
+    icon: 'ni ni-single-02 text-yellow',
     component: Profile,
-    layout: "/admin",
+    layout: '/admin',
   },
   {
-    path: "/login",
-    name: "Login",
-    icon: "ni ni-key-25 text-info",
+    path: '/login',
+    name: 'Login',
+    icon: 'ni ni-key-25 text-info',
     component: Login,
-    layout: "/auth",
+    layout: '/auth',
   },
   {
-    path: "/register",
-    name: "Register",
-    icon: "ni ni-circle-08 text-pink",
+    path: '/register',
+    name: 'Register',
+    icon: 'ni ni-circle-08 text-pink',
     component: Register,
-    layout: "/auth",
+    layout: '/auth',
   },
 ];
 export default routes;
