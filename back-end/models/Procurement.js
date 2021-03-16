0eee960e656a4588f15276f645ddc531a23674f1
ashrafwkhalil <ashrafdude@gmail.com>
2021-03-15 23:21:11 -0400

    Created Schema for Procurement
    #78

---
+++ back-end/models/Procurement.js
@@ -0,0 +1,30 @@
+const ProcurementSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    quantity: {
+        type: Number,
+        required: true
+    },
+    supplier: {
+        type: String,
+        required: true
+    },
+    destination: {
+        type: String,
+        required: true
+    },
+    value: {
+        type: Number,
+        required: true
+    },
+    date: {
+        type: Date,
+        required: true
+    }
+    
+    
+});
+
+module.exports = Procurement = mongoose.model('procurement', ProcurementSchema);
