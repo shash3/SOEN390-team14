d647b199e67bcf6c322dc28e0b3f0ad302ac94da
shash3 <shashank.p0713@gmail.com>
2021-03-17 17:00:27 -0400

    updated the test case
    updates to the tests. Now all tests pass and travis CI should build successfully #57

--- back-end/test/Sales/salesAuthTest.test.js
+++ back-end/test/Sales/salesAuthTest.test.js
@@ -2,51 +2,62 @@ const request = require("supertest");
 const app = require("../../server.js");
 
 let token = "";
-it("test logging in ", async () => {
+describe("Post Endpoints", () => {
+  it("create testing user", async () => {
+    await request(app).post("/api/users/admin").send({
+      name: "admin",
+      email: "admin@gmail.com",
+      password: "12345678",
+    });
+  });
+
+  it("test logging in ", async () => {
     const res = await request(app).post("/api/auth/login").send({
-        email: "production@gmail.com",
-        password: "12345678",
+      name: "admin",
+      email: "admin@gmail.com",
+      password: "12345678",
     });
     token = res.body.token;
-});
+  });
 
-it("receiving sales ", async () => {
+  it("receiving sales ", async () => {
     const res = await request(app).get("/api/sales");
-    
+
     expect(res.body).toBeTruthy();
-});
-it("receiving receivables ", async () => {
+  });
+  it("receiving receivables ", async () => {
     const res = await request(app).get("/api/sales/receivables");
-    
+
     expect(res.body).toBeTruthy();
-});
+  });
 
-it('tests adding a sale', async() => {
-    const body ={
-    name:"test",
-    quantity:99,
-    purchaser:"test",
-    location:"test",
-    value: 99,
-    date: Date.now(),
-    paid:false
-    }
-    const res2 = await request(app).post('/api/sales/add',{
+  it("tests adding a sale", async () => {
+    const body = {
+      name: "test",
+      quantity: 99,
+      purchaser: "test",
+      location: "test",
+      value: 99,
+      date: Date.now(),
+      paid: false,
+    };
+    const res2 = await request(app)
+      .post("/api/sales/add", {
         headers: {
-            'x-auth-token': token,
-            'Content-Type': 'application/json',
-          },
-    }).send(JSON.stringify(body));
-    
-    
-    expect(res2.body).toBeTruthy();
-})
+          "x-auth-token": token,
+          "Content-Type": "application/json",
+        },
+      })
+      .send(JSON.stringify(body));
 
-it('tests deletion', async() => {
-   
-    const res2 = await request(app).post('/api/sales/deleteN',"test").set('x-auth-token', token)
-    
     expect(res2.body).toBeTruthy();
+  });
+
+  it("tests deletion", async () => {
+    const res2 = await request(app)
+      .post("/api/sales/deleteN", "test")
+      .set("x-auth-token", token);
+
+    expect(res2.body).toBeTruthy();
+  });
 });
-
-
--- back-end/test/TransportationTest/transportationAuthTest.test.js
+++ back-end/test/TransportationTest/transportationAuthTest.test.js
@@ -3,28 +3,37 @@ const app = require("../../server.js");
 
 let token = "";
 describe("Post Endpoints", () => {
-    it("create testing user", async () => {
-        await request(app).post("/api/users/admin").send({
-        name: "admin",
-        email: "admin@gmail.com",
-        password: "12345678",
-        });
+  it("create testing user", async () => {
+    await request(app).post("/api/users/admin").send({
+      name: "admin",
+      email: "admin@gmail.com",
+      password: "12345678",
     });
+  });
 
-    it("test logging in ", async () => {
-        const res = await request(app).post("/api/auth/login").send({
-            email: "admin@gmail.com",
-            password: "12345678",
-        });
-        token = res.body.token;
-        expect(res.body.token).toBeTruthy();
-        expect(res.body.permission).toBeTruthy();
+  it("create testing user", async () => {
+    await request(app).post("/api/users/admin").send({
+      name: "admin",
+      email: "admin@gmail.com",
+      password: "12345678",
     });
-    
-    it('Gets the test endpoint', async() => {
-        // Sends GET Request to /test endpoint
-        const res2 = await request(app).get('/api/transportation').set('x-auth-token', token)
-        expect(res2.body).toBeTruthy();
-    });
+  });
 
+  it("test logging in ", async () => {
+    const res = await request(app).post("/api/auth/login").send({
+      email: "admin@gmail.com",
+      password: "12345678",
+    });
+    token = res.body.token;
+    expect(res.body.token).toBeTruthy();
+    expect(res.body.permission).toBeTruthy();
+  });
+
+  it("Gets the test endpoint", async () => {
+    // Sends GET Request to /test endpoint
+    const res2 = await request(app)
+      .get("/api/transportation")
+      .set("x-auth-token", token);
+    expect(res2.body).toBeTruthy();
+  });
 });
