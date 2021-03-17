94f03533e1a939e1a9152ed49874c0ba553a5a24
ashrafwkhalil <ashrafdude@gmail.com>
2021-03-17 15:00:00 -0400

    Update procurementAuthTest.test.js
    #27

--- back-end/test/Procurement/procurementAuthTest.test.js
+++ back-end/test/Procurement/procurementAuthTest.test.js
@@ -6,15 +6,26 @@ it("test logging in ", async () => {
         email: "production@gmail.com",
         password: "12345678",
     });
-    token = res.body.token;
+   
 });
 
 it("receiving procurements ", async () => {
+    const rest = await request(app).post("/api/auth/login").send({
+        email: "production@gmail.com",
+        password: "12345678",
+    });
+    const token = rest.body.token;
     const res = await request(app).get("/api/procurement");
     
     expect(res.body).toBeTruthy();
 });
 it("receiving payables ", async () => {
+    const rest = await request(app).post("/api/auth/login").send({
+        email: "production@gmail.com",
+        password: "12345678",
+    });
+    const token = rest.body.token;
+
     const res = await request(app).get("/api/procurement/payables");
     
     expect(res.body).toBeTruthy();
@@ -22,6 +33,12 @@ it("receiving payables ", async () => {
 
 
 it('tests adding a procurement', async() => {
+    const rest = await request(app).post("/api/auth/login").send({
+        email: "production@gmail.com",
+        password: "12345678",
+    });
+    const token = rest.body.token;
+   console.log(rest.body);
     const body ={
     name:"test",
     quantity:99,
@@ -31,18 +48,26 @@ it('tests adding a procurement', async() => {
     date: Date.now(),
     paid:false
     }
-    const res2 = await request(app).post('/api/procurement/add',{
+    const res2 = await request(app).post({
         headers: {
             'x-auth-token': token,
             'Content-Type': 'application/json',
           },
+          url : '/api/procurement/add',
+          body:body,
+          
     }).send(JSON.stringify(body));
     
-    
+    console.log(res2.body);
     expect(res2.body).toBeTruthy();
 })
 
 it('tests deletion', async() => {
+    const rest = await request(app).post("/api/auth/login").send({
+        email: "production@gmail.com",
+        password: "12345678",
+    });
+    const token = rest.body.token;
    
     const res2 = await request(app).post('/api/procurement/deleteN',"test").set('x-auth-token', token)
     
