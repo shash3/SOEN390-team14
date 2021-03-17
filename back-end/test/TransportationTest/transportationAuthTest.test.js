c44e273f2d604eb9ab94a6d47e49e36292b0a347
shash3 <shashank.p0713@gmail.com>
2021-03-17 18:48:52 -0400

    added more test to procurement

--- back-end/test/TransportationTest/transportationAuthTest.test.js
+++ back-end/test/TransportationTest/transportationAuthTest.test.js
@@ -11,24 +11,6 @@ describe("Post Endpoints", () => {
     });
   });
 
-  it("create testing user", async () => {
-    await request(app).post("/api/users/admin").send({
-      name: "admin",
-      email: "admin@gmail.com",
-      password: "12345678",
-    });
-  });
-
-  it("test logging in ", async () => {
-    const res = await request(app).post("/api/auth/login").send({
-      email: "admin@gmail.com",
-      password: "12345678",
-    });
-    token = res.body.token;
-    expect(res.body.token).toBeTruthy();
-    expect(res.body.permission).toBeTruthy();
-  });
-
   it("Gets the test endpoint", async () => {
     // Sends GET Request to /test endpoint
     const res2 = await request(app)
@@ -36,4 +18,61 @@ describe("Post Endpoints", () => {
       .set("x-auth-token", token);
     expect(res2.body).toBeTruthy();
   });
+
+  it("retrive all shipments", async () => {
+    const res1 = await request(app).get("/api/transportation/", {
+        headers: {
+          "x-auth-token": token
+        }
+      }).send({
+        name: "saddle"
+      });
+      expect(res1.body).toBeTruthy();
+  })
+
+  it("retrive all completed shipments", async () => {
+    const status = await request(app).get("/api/transportation/completed", {
+        headers: {
+          "x-auth-token": token
+        }
+      }).send({
+        status: "completed"
+      });
+      expect(status.body).toBeTruthy();
+  })
+
+  it("retrive all shipment that have been packaged", async () => {
+    const status = await request(app).get("/api/transportation/completed", {
+        headers: {
+          "x-auth-token": token
+        }
+      }).send({
+        packagingStatus: "true"
+      });
+      expect(status.body).toBeTruthy();
+  })
+
+  it("Retrieve specific shipment by name", async () => {
+    const res1 = await request(app).post("/api/transportation/", {
+        headers: {
+          "x-auth-token": token
+        }
+      }).send({
+        name: "Saddle"
+      });
+      expect(res1.body).toBeTruthy();
+  });
+
+  it("change shipment status", async () => {
+    const res1 = await request(app).post("/api/transportation/changeStatus", {
+        headers: {
+          "x-auth-token": token
+        }
+      }).send({
+        status: "complete"
+      });
+      expect(res1.body).toBeTruthy();
+  });
 });
+
+
