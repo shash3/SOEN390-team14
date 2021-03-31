3099ca40d8b281bda85d68787cce4ac75c6e66a0
j-eltayar <eltayarjames@gmail.com>
2021-03-30 20:30:27 -0400

    Started Planning #66
    Started fixing the finance page's front end to get ready to flesh out planning

--- client/src/views/components/Finance.jsx
+++ client/src/views/components/Finance.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 
 // reactstrap components
 import {
@@ -6,54 +6,94 @@ import {
   CardHeader,
   Container,
   Row,
-  Col,
+  Col, CardBody,
 } from 'reactstrap';
 // core components
 import FinanceHeader from '../../components/Headers/FinanceHeader.jsx';
+import {Bar} from "react-chartjs-2";
 
-const Finance = () => (
-  // Useless function
-  <>
-    <FinanceHeader />
-    {/* Page content */}
-    <Container className="mt--7" fluid>
 
-      {/* Dark table */}
-      <Row className="mt-5">
-        <div className="col">
-          <Card className="bg-default shadow">
-            <CardHeader className="bg-transparent border-0">
-              <h3 className="text-white mb-0">Welcome to Finance!</h3>
-            </CardHeader>
-            <Row className="mb-2 ml-2 mr-2 mt-2">
-              <Col className="">
-                <Card className="shadow">
-                  <CardHeader className="bg-transparent text-center">
-                    <h1 className=" mb-0">What&apos;s New with BIKERR?</h1>
-                  </CardHeader>
-                </Card>
-              </Col>
-            </Row>
-            <Row className="mb-2 ml-2 mr-2 mt-2">
-              <Col className="">
-                <Card className="shadow">
-                  <CardHeader className="bg-transparent text-center">
-                    <h2 className=" mb-0">Announcements</h2>
-                  </CardHeader>
-                </Card>
-              </Col>
-              <Col className="">
-                <Card className="shadow">
-                  <CardHeader className="bg-transparent text-center">
-                    <h2 className=" mb-0">Our Goals</h2>
-                  </CardHeader>
-                </Card>
-              </Col>
-            </Row>
-          </Card>
-        </div>
-      </Row>
-    </Container>
-  </>
-);
+const Finance = () => {
+
+  const [chartData, setChartData] = useState({})
+
+  const chart = () => {
+    setChartData({
+      labels: ['January','February','March','April','May','June','July','August','September','October','November','December'],
+      datasets : [
+        {
+          label:'Profits',
+          data:[10],
+          backgroundColor: ["#ffc107"],
+        }
+      ]
+    });
+  };
+
+  useEffect(()=>{
+    chart()
+  },[])
+
+  return (
+      <>
+        <FinanceHeader/>
+        {/* Page content */}
+        <Container className="mt--7" fluid>
+
+          {/* Dark table */}
+          <Row className="mt-5">
+            <div className="col">
+              <Card className="bg-default shadow">
+                <CardHeader className="bg-transparent border-0">
+                  <h3 className="text-white mb-0">Welcome to Finance!</h3>
+                </CardHeader>
+                <Row className="mb-2 ml-2 mr-2 mt-2">
+                  <Col className="">
+                    <Card className="shadow">
+                      <CardHeader className="bg-transparent text-center">
+                        <h1 className=" mb-0">Annual Goals and Statistics</h1>
+                      </CardHeader>
+                      <CardBody>
+                        <Bar data={chartData} options={{
+                          responsive: true,
+                          legend: { display: false },
+                          title: {
+                            display: true,
+                            text: 'Profits 2021'
+                          }
+                        }}/>
+                      </CardBody>
+                    </Card>
+                  </Col>
+                </Row>
+                <Row className="mb-2 ml-2 mr-2 mt-2">
+                  <Col className="">
+                    <Card className="shadow">
+                      <CardHeader className="bg-transparent text-center">
+                        <h1 className=" mb-0">Monthly Planning</h1>
+                        <p>Expected Sales  "Entered"</p>
+                        <p>Expected Procurement Costs  "Entered"</p>
+                        <p>Expected operating costs "Entered"</p>
+                        <p>Calculated profits "Calculated"</p>
+                      </CardHeader>
+                    </Card>
+                  </Col>
+                  <Col className="">
+                    <Card className="shadow">
+                      <CardHeader className="bg-transparent text-center">
+                        <h1 className=" mb-0">Monthly Breakdown</h1>
+                        <p>Total Sales/Cost/Profit</p>
+                        <p>Sales: Dollar Amount/# Bikes Sold</p>
+                        <p>Cost: Operational Costs+Hours/Procurement Costs</p>
+                      </CardHeader>
+                    </Card>
+                  </Col>
+                </Row>
+              </Card>
+            </div>
+          </Row>
+        </Container>
+      </>
+  );
+};
 export default Finance;
