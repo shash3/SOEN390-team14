17965ec52534104d017dc0a00af7870ee28081d7
ashrafwkhalil <ashrafdude@gmail.com>
2021-04-01 20:47:33 -0400

    created planning api and created production planning api call
    #66

---
+++ back-end/routes/api/Planning/planningAuth.js
@@ -0,0 +1,31 @@
+const express = require("express");
+
+const router = express.Router();
+const Procurement = require("../../../models/Procurement");
+const Sales = require("../../../models/Sales");
+const auth = require("../../../middleware/auth");
+const fs = require('fs');
+
+const prodPlanFile = './logs/plannedProduction.json';
+const salesPlanFile = './logs/plannedSales.json';
+
+
+
+router.post("/prod", auth, async (req,res) =>{
+    
+try{
+    fs.readFile(prodPlanFile, 'utf8', (err, data) => {
+        if (data === undefined) {
+          data = '{}';
+        }
+        res.send(JSON.parse(data));       
+      });
+}
+catch (err) {
+     
+        console.error(err.message);
+        res.status(500).send("Server Error");
+}
+})
+
+module.exports = router;
