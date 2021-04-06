5cf96a4c6af8dab548886c9cf7af7016246d0dd4
Sacha Elkaim <sachaelkaim87@gmail.com>
2021-04-06 19:33:58 -0400

    removed page links #146
    page titles from tabs would link back to auth/login. it is now removed

--- client/src/components/Navbars/AdminNavbar.jsx
+++ client/src/components/Navbars/AdminNavbar.jsx
@@ -86,12 +86,12 @@ const AdminNavbar = (props) => {
     <>
       <Navbar className="navbar-top navbar-dark" expand="md" id="navbar-main">
         <Container fluid>
-          <Link
+          <div
             className="h4 mb-0 text-white text-uppercase d-none d-lg-inline-block"
-            to="/"
+            
           >
             {props.brandText}
-          </Link>
+          </div>
           <Form className="navbar-search navbar-search-dark form-inline mr-3 d-none d-md-flex ml-lg-auto">
             <FormGroup className="mb-0">
               <InputGroup className="input-group-alternative">
