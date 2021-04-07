a30023fdfecf290329890b54a4d07765e4f54408
Sacha Elkaim <sachaelkaim87@gmail.com>
2021-04-06 20:09:49 -0400

    fixed menu-items/link #146

--- client/src/components/Navbars/AdminNavbar.jsx
+++ client/src/components/Navbars/AdminNavbar.jsx
@@ -123,26 +123,6 @@ const AdminNavbar = (props) => {
                 </Media>
               </DropdownToggle>
               <DropdownMenu className="dropdown-menu-arrow" right>
-                <DropdownItem className="noti-title" header tag="div">
-                  <h6 className="text-overflow m-0">Welcome!</h6>
-                </DropdownItem>
-                <DropdownItem to="/admin/user-profile" tag={Link}>
-                  <i className="ni ni-single-02" />
-                  <span>My profile</span>
-                </DropdownItem>
-                <DropdownItem to="/admin/user-profile" tag={Link}>
-                  <i className="ni ni-settings-gear-65" />
-                  <span>Settings</span>
-                </DropdownItem>
-                <DropdownItem to="/admin/user-profile" tag={Link}>
-                  <i className="ni ni-calendar-grid-58" />
-                  <span>Activity</span>
-                </DropdownItem>
-                <DropdownItem to="/admin/user-profile" tag={Link}>
-                  <i className="ni ni-support-16" />
-                  <span>Support</span>
-                </DropdownItem>
-                <DropdownItem divider />
                 <DropdownItem onClick={(e) => loggingOut(e)}>
                   <i className="ni ni-user-run" />
                   <span>Logout</span>
