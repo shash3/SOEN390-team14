8c904f9fe8bc6008f321f99277e9984886ccd0dd
DerekRuiz <47483376+DerekRuiz@users.noreply.github.com>
2021-04-07 13:14:36 -0400

    Reformatted Planning To Keep Convention

--- back-end/routes/api/Planning/planningAuth.js
+++ back-end/routes/api/Planning/planningAuth.js
@@ -1,105 +1,104 @@
-const express = require("express");
+const express = require('express')
 
-const router = express.Router();
-const Procurement = require("../../../models/Procurement");
-const Sales = require("../../../models/Sales");
-const auth = require("../../../middleware/auth");
-const fs = require('fs');
+const router = express.Router()
+const fs = require('fs')
+const Procurement = require('../../../models/Procurement')
+const Sales = require('../../../models/Sales')
+const auth = require('../../../middleware/auth')
 
-const prodPlanFile = './logs/plannedProduction.json';
-const salesPlanFile = './logs/plannedSales.json';
-const prodActualFile = './logs/machineOperations.json';
-const salesActualFile = './logs/salesLog.json';
+const prodPlanFile = './logs/plannedProduction.json'
+const salesPlanFile = './logs/plannedSales.json'
+const prodActualFile = './logs/machineOperations.json'
+const salesActualFile = './logs/salesLog.json'
 
-
-
-router.get("/prod", auth, async (req,res) =>{
-try{
+router.get('/prod', auth, async (req, res) => {
+  try {
     fs.readFile(prodPlanFile, 'utf8', (err, data) => {
-        if (data === undefined) {
-          data = '{}';
-        }
-        res.send(JSON.parse(data));       
-      });
-}
-catch (err) {
-     
-        console.error(err.message);
-        res.status(500).send("Server Error");
-}
+      if (data === undefined) {
+        data = '{}'
+      }
+      res.send(JSON.parse(data))
+    })
+  } catch (err) {
+    console.error(err.message)
+    res.status(500).send('Server Error')
+  }
 })
 
-router.get("/sales", auth, async (req,res) =>{
-    try{
-        fs.readFile(salesPlanFile, 'utf8', (err, data) => {
-            if (data === undefined) {
-              data = '{}';
-            }
-            res.send(JSON.parse(data));       
-          });
-    }
-    catch (err) {
-         
-            console.error(err.message);
-            res.status(500).send("Server Error");
-    }
+router.get('/sales', auth, async (req, res) => {
+  try {
+    fs.readFile(salesPlanFile, 'utf8', (err, data) => {
+      if (data === undefined) {
+        data = '{}'
+      }
+      res.send(JSON.parse(data))
+    })
+  } catch (err) {
+    console.error(err.message)
+    res.status(500).send('Server Error')
+  }
 })
-router.get("/prodActual", auth, async (req,res) =>{
-    try{
-        fs.readFile(prodActualFile, 'utf8', (err, data) => {
-            if (data === undefined) {
-              data = '{}';
-            }
-            res.send(JSON.parse(data));       
-          });
-    }
-    catch (err) {
-         
-            console.error(err.message);
-            res.status(500).send("Server Error");
-    }
-})    
-router.get("/salesActual", auth, async (req,res) =>{
-    try{
-        fs.readFile(salesActualFile, 'utf8', (err, data) => {
-            if (data === undefined) {
-              data = '{}';
-            }
-            res.send(JSON.parse(data));       
-          });
-    }
-    catch (err) {
-         
-            console.error(err.message);
-            res.status(500).send("Server Error");
-    }
-})    
-router.post("/addPlanProd", auth, async (req,res) => {
-   console.log("hello");
-        const { data } = req.body;
-        const dataStr = JSON.stringify(data, null, 2);
-        try {
-          fs.writeFile(prodPlanFile, dataStr, 'utf8', () => {});
-          res.send(true);
-        } catch (err) {
-          console.error(err.message);
-          res.status(500).send("Server Error");
-        }
-  
-});  
+router.get('/prodActual', auth, async (req, res) => {
+  try {
+    fs.readFile(prodActualFile, 'utf8', (err, data) => {
+      if (data === undefined) {
+        data = '{}'
+      }
+      res.send(JSON.parse(data))
+    })
+  } catch (err) {
+    console.error(err.message)
+    res.status(500).send('Server Error')
+  }
+})
+router.get('/salesActual', auth, async (req, res) => {
+  try {
+    fs.readFile(salesActualFile, 'utf8', (err, data) => {
+      if (data === undefined) {
+        data = '{}'
+      }
+      res.send(JSON.parse(data))
+    })
+  } catch (err) {
+    console.error(err.message)
+    res.status(500).send('Server Error')
+  }
+})
+router.post('/addPlanProd', auth, async (req, res) => {
+  console.log('hello')
+  const { data } = req.body
+  const dataStr = JSON.stringify(data, null, 2)
+  try {
+    fs.writeFile(prodPlanFile, dataStr, 'utf8', () => {})
+    res.send(true)
+  } catch (err) {
+    console.error(err.message)
+    res.status(500).send('Server Error')
+  }
+})
 
-router.post("/addPlanSales", auth, async (req,res) => {
-   console.log("hi");
-    const { data } = req.body;
-    const dataStr = JSON.stringify(data, null, 2);
-    try {
-      fs.writeFile(salesPlanFile, dataStr, 'utf8', () => {});
-    } catch (err) {
-      console.error(err.message);
-      res.status(500).send("Server Error");
-    }
+router.post('/addPlanSales', auth, async (req, res) => {
+  console.log('hi')
+  const { data } = req.body
+  const dataStr = JSON.stringify(data, null, 2)
+  try {
+    fs.writeFile(salesPlanFile, dataStr, 'utf8', () => {})
+  } catch (err) {
+    console.error(err.message)
+    res.status(500).send('Server Error')
+  }
+})
 
-});  
-        
+// Write the log file for machine
+router.post('/addProdActual', auth, (req, res) => {
+  const { data } = req.body
+  const dataStr = JSON.stringify(data, null, 2)
+  try {
+    fs.writeFile(prodActualFile, dataStr, 'utf8', () => {})
+  } catch (err) {
+    console.error(err.message)
+    res.status(500).send('Server Error')
+  }
+})
 
-module.exports = router;
+module.exports = router
