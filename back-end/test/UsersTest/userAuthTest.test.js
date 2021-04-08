0693d4b613b7f90e7a12fcc151a6b139bbc9766d
j-eltayar <eltayarjames@gmail.com>
2021-04-07 23:51:00 -0400

    Fix Tests #27
    Added tests

--- back-end/test/UsersTest/userAuthTest.test.js
+++ back-end/test/UsersTest/userAuthTest.test.js
@@ -46,3 +46,72 @@ describe("Get Endpoints", () => {
     expect(res2.body).toBeTruthy();
   });
 });
+
+it("planned Production", async () => {
+    const res = await request(app).get("/api/Planning/prod", {
+        headers: {
+            "x-auth-token": token,
+        },
+    });
+    expect(res.body).toBeTruthy();
+});
+
+it("planned Sales", async () => {
+    const res = await request(app).get("/api/Planning/sales", {
+        headers: {
+            "x-auth-token": token,
+        },
+    });
+    expect(res.body).toBeTruthy();
+});
+
+it("planned Production actual", async () => {
+    const res = await request(app).get("/api/Planning/prodActual", {
+        headers: {
+            "x-auth-token": token,
+        },
+    });
+    expect(res.body).toBeTruthy();
+});
+
+it("planned Sales actual", async () => {
+    const res = await request(app).get("/api/Planning/salesActual", {
+        headers: {
+            "x-auth-token": token,
+        },
+    });
+    expect(res.body).toBeTruthy();
+});
+
+it("Add plan Production", async () => {
+    const res1 = await request(app).post("/api/Planning/addPlanProd", {
+        headers: {
+            "x-auth-token": token
+        }
+    }).send({
+        data: "{}"
+    });
+    expect(res1.body).toBeTruthy();
+});
+
+it("Add plan Sales", async () => {
+    const res1 = await request(app).post("/api/Planning/addPlanSales", {
+        headers: {
+            "x-auth-token": token
+        }
+    }).send({
+        data: "{}"
+    });
+    expect(res1.body).toBeTruthy();
+});
+
+it("Add Production Actual", async () => {
+    const res1 = await request(app).post("/api/Planning/addProdActual", {
+        headers: {
+            "x-auth-token": token
+        }
+    }).send({
+        data: "{}"
+    });
+    expect(res1.body).toBeTruthy();
+});
