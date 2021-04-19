0550a322330eec8b024f165063401cf1ffd01a78
shash3 <shashank.p0713@gmail.com>
2021-04-19 15:58:01 -0400

    update jest

--- client/test/financeTest.test.js
+++ client/test/financeTest.test.js
@@ -14,29 +14,29 @@ describe('FinanceTest', () => {
   let vars
   beforeEach(async () => {
     driver = await new webdriver.Builder()
-    .forBrowser('chrome')
-    .setChromeOptions(
-      new chrome.Options().addArguments([
-        '--headless',
-        '--no-sandbox',
-        '--disable-dev-shm-usage',
-      ]),
-    )
-    .build()
+      .forBrowser('chrome')
+      .setChromeOptions(
+        new chrome.Options().addArguments([
+          '--headless',
+          '--no-sandbox',
+          '--disable-dev-shm-usage',
+        ]),
+      )
+      .build()
     vars = {}
   })
   afterEach(async () => {
-    await driver.quit();
+    await driver.quit()
   })
   it('FinanceTest', async () => {
-    await driver.get("https://bikerr.herokuapp.com/auth/login")
+    await driver.get('https://bikerr.herokuapp.com/auth/login')
     await driver.manage().window().setRect(1098, 678)
-    await driver.findElement(By.name("email")).click()
-    await driver.findElement(By.name("email")).sendKeys("admin@gmail.com")
-    await driver.findElement(By.name("password")).click()
-    await driver.findElement(By.name("password")).sendKeys("12345678")
-    await driver.findElement(By.css(".my-4")).click()
-    await driver.findElement(By.css(".nav-item:nth-child(4) > .nav-link")).click()
+    await driver.findElement(By.name('email')).click()
+    await driver.findElement(By.name('email')).sendKeys('admin@gmail.com')
+    await driver.findElement(By.name('password')).click()
+    await driver.findElement(By.name('password')).sendKeys('12345678')
+    await driver.findElement(By.css('.my-4')).click()
+    // await driver.findElement(By.css(".nav-item:nth-child(4) > .nav-link")).click()
     await driver.close()
   })
 })
